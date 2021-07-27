27fe3386d252e5b890f1cb58cc22c012b5be8c02
Ismail Hosen <ismail27.dec@gmail.com>
2021-07-27 21:01:14 +0600

    Allow Cors And Check Login Succes

--- JWTAuthenticationWithNetCore/JWTAuthenticationWithNetCore/Startup.cs
+++ JWTAuthenticationWithNetCore/JWTAuthenticationWithNetCore/Startup.cs
@@ -66,12 +66,18 @@ namespace JWTAuthenticationWithNetCore
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Key"]))
                     };
                 });
+
+            services.AddCors();
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(opt =>
+
+                opt.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+            );
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
--- jwt-authentication-react-app/src/App.js
+++ jwt-authentication-react-app/src/App.js
@@ -1,8 +1,66 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      login: false,
+      store: null,
+    };
+  
+  }
+  
+  EmailOnChangeHandler = (e) => {
+    this.setState({
+      email: e.target.value,
+    });
+  };
+
+  PasswordOnChangeHandler = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
+
+  login = () => {
+    let axiosConfig = {
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+        "Access-Control-Allow-Origin": "*",
+      },
+    };
+    let data = {
+      Email: this.state.email,
+      Password: this.state.password
+    }
+    axios
+      .post("https://localhost:5001/api/user/token", JSON.stringify(data), axiosConfig)
+      .then(response => console.log(response))
+      .catch((error) => console.log("Error: ", error));
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h1>Jwt Token with React</h1>
+        <div>
+          <input
+            type="email"
+            placeholder="email"
+            onChange={(event) => this.EmailOnChangeHandler(event)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            onChange={(event) => this.PasswordOnChangeHandler(event)}
+          />
+          <button onClick={this.login}>Login</button>
+        </div>
+      </div>
+    );
   }
 }
 
