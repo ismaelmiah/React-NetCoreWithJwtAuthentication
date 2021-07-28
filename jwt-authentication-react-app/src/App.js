a85489f782c8e0671cb61b4f328c16f32a536f90
Ismail Hosen <ismail27.dec@gmail.com>
2021-07-28 23:47:21 +0600

    Jwt Token Saved into localStorage

--- jwt-authentication-react-app/src/App.js
+++ jwt-authentication-react-app/src/App.js
@@ -2,17 +2,31 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       email: "",
       password: "",
       login: false,
       store: null,
+      post: "",
+      posts: [],
     };
-  
   }
-  
+
+  componentDidMount() {
+    const localObj = JSON.parse(localStorage.getItem("login"));
+    if (localObj != null) {
+      this.setState({
+        login: true,
+        store: {
+          username: localObj.username,
+          token: localObj.token,
+        },
+      });
+    }
+  }
+
   EmailOnChangeHandler = (e) => {
     this.setState({
       email: e.target.value,
@@ -34,31 +48,92 @@ class App extends Component {
     };
     let data = {
       Email: this.state.email,
-      Password: this.state.password
-    }
+      Password: this.state.password,
+    };
     axios
-      .post("https://localhost:5001/api/user/token", JSON.stringify(data), axiosConfig)
-      .then(response => console.log(response))
+      .post(
+        "https://localhost:5001/api/user/token",
+        JSON.stringify(data),
+        axiosConfig
+      )
+      .then((response) => {
+        localStorage.setItem(
+          "login",
+          JSON.stringify({
+            login: true,
+            token: response.data.token,
+            username: this.state.username,
+          })
+        );
+        this.setState({ login: true });
+      })
       .catch((error) => console.log("Error: ", error));
   };
 
+  PostOnChangeHanlder = (e) => {
+    this.setState({
+      post: e.target.value,
+    });
+  };
+
+  post = () => {
+    const post = this.state.post;
+    let Oldposts = [...this.state.posts];
+    Oldposts.push({
+      post: post,
+      id: Oldposts.length + 1,
+    });
+    this.setState({
+      posts: Oldposts,
+      post: "",
+    });
+  };
+
+  logout = () => {
+    localStorage.clear();
+    this.setState({
+      login: false
+    })
+  }
   render() {
     return (
       <div>
         <h1>Jwt Token with React</h1>
-        <div>
-          <input
-            type="email"
-            placeholder="email"
-            onChange={(event) => this.EmailOnChangeHandler(event)}
-          />
-          <input
-            type="password"
-            placeholder="password"
-            onChange={(event) => this.PasswordOnChangeHandler(event)}
-          />
-          <button onClick={this.login}>Login</button>
-        </div>
+        {!this.state.login ? (
+          <div>
+            <input
+              type="email"
+              placeholder="email"
+              onChange={(event) => this.EmailOnChangeHandler(event)}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              onChange={(event) => this.PasswordOnChangeHandler(event)}
+            />
+            <button onClick={this.login}>Login</button>
+          </div>
+        ) : (
+          <div>
+            <div>
+              <div>
+                <button onClick={this.logout}>Logout</button>
+              </div>
+              <textarea
+                onChange={(event) => this.PostOnChangeHanlder(event)}
+                value={this.state.post}
+              ></textarea>
+              <button onClick={this.post}>Post</button>
+            </div>
+            <div>
+              {this.state.posts.map((p) => (
+                <div key={p.id}>
+                  <p>{p.post}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     );
   }
